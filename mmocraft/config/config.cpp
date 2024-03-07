d600d664a954f0270b7bc783fbc91c737f3a74ba
kirasys <kirasys0@naver.com>
2024-03-07 13:46:30 +0900

    config 관련 코드 추가

---
+++ mmocraft/config/config.cpp
@@ -0,0 +1,88 @@
+#include "config.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <string>
+#include <string_view>
+#include <algorithm>
+
+namespace {
+	void trim(std::string_view line) noexcept {
+		line.remove_prefix(std::min(line.find_first_not_of(' '), line.size()));
+		if (line.back() == ' ')
+			line.remove_suffix(line.size() - line.find_last_not_of(' ') - 1);
+	}
+}
+
+namespace config {
+	using ConfigMap = std::map<std::string, std::string>;
+
+	// global variables
+
+	const char* logging_config_filepath = "config/configures/logging.conf";
+	Configuration configuration;
+
+	// private functions
+
+	ConfigMap parse_config(std::stringstream& config_ss) {
+		ConfigMap config_map;
+
+		std::string line;
+		while (std::getline(config_ss >> std::ws, line)) {
+			if (line.size() && line[0] == '#')
+				continue;
+
+			auto colon_pos = line.find(':');
+			if (colon_pos == std::string::npos)
+				continue;
+
+			std::string_view key = line;
+			key.remove_suffix(key.size() - colon_pos);
+			trim(key);
+
+			std::string_view value = line;
+			value.remove_prefix(colon_pos + 1);
+			trim(value);
+
+			config_map[std::string(key)] = std::string(value);
+		}
+
+		return config_map;
+	}
+
+	std::tuple<ConfigMap, bool> read_config(const char* config_filepath) {
+		std::ifstream config_stm(config_filepath, std::ifstream::in);
+
+		if (config_stm.fail())
+			return { {}, false };
+
+		std::stringstream config_ss;
+		config_ss << config_stm.rdbuf();
+
+		return { parse_config(config_ss), true };
+	}
+
+	// public functions
+
+	bool load_config() {
+		auto [config_map, success] = read_config(logging_config_filepath);
+		if (!success) return false;
+
+		configuration.loaded = true;
+
+		if (config_map.find("log_level") != config_map.end())
+			configuration.log.level = Logging::string_to_level(config_map["log_level"]);
+		if (config_map.find("log_file_path") != config_map.end())
+			configuration.log.file_path = config_map["log_file_path"];
+
+		return true;
+	}
+
+	const Configuration& get_config() {
+		if (!configuration.loaded)
+			load_config();
+		return configuration;
+	}
+}
---
+++ mmocraft/config/config.h
@@ -0,0 +1,36 @@
+#pragma once
+#pragma once
+#include <string>
+#include <tuple>
+
+#include "../logging/logger.h"
+
+namespace config {
+	struct Configuration {
+		bool loaded;
+
+		struct LogConfig {
+			Logging::LogLevel level = Logging::LogLevel::kInfo;
+			std::string file_path = "log\\";
+		} log;
+
+		struct LoginConfig {
+
+		} login;
+
+		Configuration() :loaded(false)
+		{
+
+		}
+
+		// only one global configuration variable exists.
+		Configuration(Configuration&) = delete;
+		Configuration(Configuration&&) = delete;
+		Configuration& operator=(Configuration&) = delete;
+		Configuration& operator=(Configuration&&) = delete;
+	};
+
+	bool load_config();
+
+	const Configuration& get_config();
+}
