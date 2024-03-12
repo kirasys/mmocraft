339d86b2fa09e775df5eb39ed89b78e1d0d3d532
kirasys <kirasys0@naver.com>
2024-03-12 16:58:27 +0900

    feat: 콘솔에 에러 출력하는 함수 추가

--- mmocraft/logging/error.cpp
+++ mmocraft/logging/error.cpp
@@ -1,7 +1,39 @@
-#pragma once
 #include "error.h"
+
+#include <map>
+#include <filesystem>
 
 namespace logging
 {
+	const std::map<ErrorMessage, const char*> error_msg_map = {
+		{ErrorMessage::INVALID_SOCKET_ERROR, "Invalid socket"}
+	};
+	
+	ErrorStream::ErrorStream(const std::source_location& location)
+	{
+		m_buf << std::filesystem::path(location.file_name()).filename() << '('
+			<< location.line() << ':'
+			<< location.column() << ") `"
+			<< location.function_name() << "`: ";
+	}
 
+	ErrorStream& ErrorStream::operator<<(ErrorMessage type)
+	{
+		if (error_msg_map.find(type) == error_msg_map.end())
+			m_buf << "Unknown error type (" << type << ')';
+		else
+			m_buf << error_msg_map.at(type);
+
+		return *this;
+	}
+
+	ErrorStream::~ErrorStream()
+	{
+		// TODO: does it thread-safe?
+		std::cerr << m_buf.str() << std::endl;
+	}
+
+	ErrorStream cerr(const std::source_location& location) {
+		return ErrorStream{ location };
+	}
 }
--- mmocraft/logging/error.h
+++ mmocraft/logging/error.h
@@ -1,9 +1,41 @@
 #pragma once
 
+#include <iostream>
+#include <sstream>
+#include <utility>
+#include <source_location>
+
 namespace logging
 {
-	void print_console()
-	{
+	enum ErrorMessage {
+		INVALID_SOCKET_ERROR
+	};
 
-	}
+	class ErrorStream
+	{
+	public:
+		ErrorStream(const std::source_location&);
+
+		~ErrorStream();
+
+		// template member function should be in the header.
+		template <typename T>
+		ErrorStream& operator<<(T&& value)
+		{
+			m_buf << std::forward<T>(value);
+			return *this;
+		}
+
+		ErrorStream& operator<<(ErrorMessage);
+
+		// no move/copy
+		ErrorStream(ErrorStream&) = delete;
+		ErrorStream(ErrorStream&&) = delete;
+		ErrorStream& operator=(ErrorStream&) = delete;
+		ErrorStream& operator=(ErrorStream&&) = delete;
+	private:
+		std::stringstream m_buf;
+	};
+
+	ErrorStream cerr(const std::source_location& location = std::source_location::current());
 }
