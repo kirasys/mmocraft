fc4e4b853ea58a6b5df21f4f074801b82d205ad1
kirasys <kirasys0@naver.com>
2024-03-12 14:33:23 +0900

    Revert "style: namespace 이름 변경 (logging -> log)"
    This reverts commit bc6e9a9ab6e67ba3e608976e4969091893075fca.

--- mmocraft/config/config.h
+++ mmocraft/config/config.h
@@ -9,7 +9,7 @@ namespace config {
 		bool loaded;
 
 		struct LogConfig {
-			log::LogLevel level = log::LogLevel::Info;
+			Logging::LogLevel level = Logging::LogLevel::kInfo;
 			std::string file_path = "log\\";
 		} log;
 
---
+++ mmocraft/logging/logger.cpp
@@ -0,0 +1,37 @@
+#include "Logger.h"
+
+#include <iostream>
+#include <map>
+
+namespace Logging {
+	LogLevel string_to_level(std::string log_level) {
+		static std::map<std::string, LogLevel> log_level_map = {
+			{"DEBUG", LogLevel::Debug},
+			{"INFO", LogLevel::Info},
+			{"WARN", LogLevel::Warn},
+			{"ERROR", LogLevel::Error},
+			{"FATAL", LogLevel::Fatal},
+		};
+		
+		if (log_level_map.find(log_level) == log_level_map.end())
+			return LogLevel::Info; // Default
+
+		return log_level_map[log_level];
+	}
+
+	Logger::Logger(const char* log_output_file = nullptr) {
+		if (log_output_file) {
+			m_log_stream.open(log_output_file, std::ofstream::out);
+			if (m_log_stream.fail()) {
+				std::cerr << "Error opening file: " << log_output_file;
+				return;
+			}
+		}
+	}
+
+	Logger::~Logger() {
+		if (m_log_stream.is_open())
+			m_log_stream.close();
+
+	}
+}
