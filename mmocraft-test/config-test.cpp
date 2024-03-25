799b08a8d32616ac60b64f039e574a72a4337622
kirasys <kirasys0@naver.com>
2024-03-25 23:56:16 +0900

    test: config 테스트 삭제

--- mmocraft-test/config-test.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include "../mmocraft/config/config.cpp"
-#include "../mmocraft/logging/logger.cpp"
-
-TEST(ConfigTest, GetConfig) {
-	decltype(auto) conf = config::get_config();
-
-	EXPECT_TRUE(conf.loaded);
-}
