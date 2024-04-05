75ae485c5cb68fedce84042f75d6348a26fe84c0
kirasys <kirasys0@naver.com>
2024-04-05 20:11:13 +0900

    refactor: time 관련 유틸을 위한 헤더 마련

--- mmocraft/util/common_util.h
+++ mmocraft/util/common_util.h
@@ -1,4 +1,6 @@
 #pragma once
+
 #include "deferred_call.h"
 #include "noncopyable.h"
 #include "custom_unique_ptr.h"
+#include "time_util.h"
---
+++ mmocraft/util/time_util.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <chrono>
+#include <ctime>
+
+namespace util
+{
+	inline std::time_t current_timestmap()
+	{
+		return std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	}
+}
