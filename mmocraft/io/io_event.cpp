733c5e348c0834b6e420a07c0a73d2f325a8b596
kirasys <kirasys0@naver.com>
2024-04-27 22:13:15 +0900

    refactor: invoke_handler 구현 코드를 cpp 파일로 이동

--- mmocraft/io/io_event.cpp
+++ mmocraft/io/io_event.cpp
@@ -3,5 +3,32 @@
 
 namespace io
 {
-	
+	void IoAcceptEvent::invoke_handler(IoEventHandler* event_handler, DWORD transferred_bytes)
+	{
+		if (event_handler->handle_io_event(event_type).has_value())
+			event_handler->on_success();
+		else
+			event_handler->on_error();
+	}
+
+	void IoRecvEvent::invoke_handler(IoEventHandler* event_handler, DWORD transferred_bytes)
+	{
+		// pre-processing
+		if (transferred_bytes == 0)	// EOF
+			return event_handler->on_error();
+
+		buffer_size += transferred_bytes;
+
+		// deliver events to the owner.
+		auto processed_bytes = event_handler->handle_io_event(event_type);
+
+		// post-processing
+		if (not processed_bytes.has_value())
+			return event_handler->on_error();
+
+		if (buffer_size -= processed_bytes.value())
+			std::memmove(buffer, buffer + processed_bytes.value(), buffer_size);
+
+		event_handler->on_success();
+	}
 }
--- mmocraft/io/io_event.h
+++ mmocraft/io/io_event.h
@@ -60,13 +60,7 @@ namespace io
 
 		std::uint8_t buffer[1024];
 
-		void invoke_handler(IoEventHandler* event_handler, DWORD transferred_bytes) override
-		{
-			if (event_handler->handle_io_event(event_type).has_value())
-				event_handler->on_success();
-			else
-				event_handler->on_error();
-		}
+		void invoke_handler(IoEventHandler* event_handler, DWORD transferred_bytes) override;
 	};
 
 	struct IoRecvEvent : IoEvent
@@ -96,26 +90,7 @@ namespace io
 			return buffer + sizeof(buffer);
 		}
 		
-		void invoke_handler(IoEventHandler* event_handler, DWORD transferred_bytes) override
-		{
-			// pre-processing
-			if (transferred_bytes == 0)	// EOF
-				return event_handler->on_error();
-
-			buffer_size += transferred_bytes;
-
-			// deliver events to the owner.
-			auto processed_bytes = event_handler->handle_io_event(event_type);
-
-			// post-processing
-			if (not processed_bytes.has_value())
-				return event_handler->on_error();
-			
-			if (buffer_size -= processed_bytes.value())
-				std::memmove(buffer, buffer + processed_bytes.value(), buffer_size);
-
-			event_handler->on_success();
-		}
+		void invoke_handler(IoEventHandler* event_handler, DWORD transferred_bytes) override;
 	};
 
 	struct IoSendEvent : IoEvent
