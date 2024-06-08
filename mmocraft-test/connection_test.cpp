d5ccbd64ce0456a9883f2dc659330cd99993807c
kirasys <kirasys0@naver.com>
2024-06-08 23:42:26 +0900

    test: io recv 이벤트를 올바르게 처리하는지 확인하는 테스트 추가

--- mmocraft-test/connection_test.cpp
+++ mmocraft-test/connection_test.cpp
@@ -35,7 +35,7 @@ protected:
     net::PacketHandleServerStub handle_server_stub;
     net::Connection SUT_connection;
 };
-TEST_F(ConnectionTest, Request_Recv_Event_Correctly)
+TEST_F(ConnectionTest, Activate_Recv_Event_Correctly)
 {
     io::IoRecvEventData io_recv_data;
     io::IoRecvEvent io_recv_event(&io_recv_data);
@@ -49,7 +49,7 @@ TEST_F(ConnectionTest, Request_Recv_Event_Correctly)
     EXPECT_EQ(mock.get_recv_bytes(), recv_buffer_space);
     EXPECT_EQ(mock.get_recv_buffer(), recv_buffer_start);
 
-    // if receives 100 bytes
+    // if receives only 100 bytes
     io_recv_data.push(nullptr, 100);
     SUT_connection.descriptor.activate_receive_cycle(&io_recv_event);
 
@@ -58,7 +58,7 @@ TEST_F(ConnectionTest, Request_Recv_Event_Correctly)
     EXPECT_EQ(mock.get_recv_buffer(), recv_buffer_start + 100);
 }
 
-TEST_F(ConnectionTest, Request_Send_Event_Correctly)
+TEST_F(ConnectionTest, Activate_Send_Event_Correctly)
 {
     io::IoSendEventData io_send_data;
     io::IoSendEvent io_send_event(&io_send_data);
@@ -81,4 +81,29 @@ TEST_F(ConnectionTest, Request_Send_Event_Correctly)
     EXPECT_TRUE(io_send_event.is_processing);
     EXPECT_EQ(mock.get_send_bytes(), 50);
     EXPECT_EQ(mock.get_send_buffer(), send_buffer_start + 50);
+}
+
+TEST_F(ConnectionTest, Handle_Recv_Event_Correctly)
+{
+    io::IoRecvEventData io_recv_data;
+    io::IoRecvEvent io_recv_event(&io_recv_data);
+
+    std::byte set_block_packet[]{ std::byte(0x05),                    // packet id
+                                  std::byte(0x01), std::byte(0x00),   // x
+                                  std::byte(0x02), std::byte(0x00),   // y
+                                  std::byte(0x03), std::byte(0x00),   // z
+                                  std::byte(1),                       // mode
+                                  std::byte(0)};                      // block_type
+
+    // write three set block packets to the buffer.
+    auto recv_buffer_start = io_recv_data.data();
+    for (int i = 0; i < 3; i++)
+        std::memcpy(recv_buffer_start + i * sizeof(set_block_packet), set_block_packet, sizeof(set_block_packet));
+
+    io_recv_event.invoke_handler(SUT_connection, sizeof(set_block_packet) * 3);
+
+    EXPECT_TRUE(SUT_connection.get_last_error().is_strong_success())
+        << "Unexpected error:" << SUT_connection.get_last_error().to_string();
+    EXPECT_EQ(mock.get_recv_times(), 1);
+    EXPECT_EQ(io_recv_data.size(), 0);
 }
