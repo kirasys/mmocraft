16bd566734ee19247a446e08e5d9be8f313fd7fb
kirasys <kirasys0@naver.com>
2024-06-09 10:15:07 +0900

    test: parital pacet read가 발생했을 때 정상적으로 작동하는 지 확인하는 테스트 추가

--- mmocraft-test/connection_test.cpp
+++ mmocraft-test/connection_test.cpp
@@ -58,6 +58,18 @@ TEST_F(ConnectionTest, Activate_Recv_Event_Correctly)
     EXPECT_EQ(mock.get_recv_buffer(), recv_buffer_start + 100);
 }
 
+TEST_F(ConnectionTest, Activate_Recv_Event_Fail_Insuffient_Buffer)
+{
+    io::IoRecvEventData io_recv_data;
+    io::IoRecvEvent io_recv_event(&io_recv_data);
+
+    io_recv_data.push(nullptr, io::RECV_BUFFER_SIZE);
+    SUT_connection.descriptor.activate_receive_cycle(&io_recv_event);
+
+    EXPECT_TRUE(not io_recv_event.is_processing);
+    EXPECT_EQ(mock.get_recv_times(), 0);
+}
+
 TEST_F(ConnectionTest, Activate_Send_Event_Correctly)
 {
     io::IoSendEventData io_send_data;
@@ -106,4 +118,28 @@ TEST_F(ConnectionTest, Handle_Recv_Event_Correctly)
         << "Unexpected error:" << SUT_connection.get_last_error().to_string();
     EXPECT_EQ(mock.get_recv_times(), 1);
     EXPECT_EQ(io_recv_data.size(), 0);
+}
+
+TEST_F(ConnectionTest, Handle_Recv_Event_Partial_Packet_Correctly)
+{
+    io::IoRecvEventData io_recv_data;
+    io::IoRecvEvent io_recv_event(&io_recv_data);
+
+    std::byte set_block_partial_packet[]{ std::byte(0x05),            // packet id
+                                  std::byte(0x01), std::byte(0x00),   // x
+                                  std::byte(0x02), std::byte(0x00),   // y
+                                  std::byte(0x03), std::byte(0x00),   // z
+                                  std::byte(1),                       // mode
+                               /* std::byte(0) */};                   // block_type
+
+    // write three set block packets to the buffer.
+    auto recv_buffer_start = io_recv_data.data();
+    std::memcpy(recv_buffer_start, set_block_partial_packet, sizeof(set_block_partial_packet));
+
+    io_recv_event.invoke_handler(SUT_connection, sizeof(set_block_partial_packet));
+
+    // expect going to receive nonetheless insuffient packet data error.
+    EXPECT_EQ(mock.get_recv_times(), 1);
+    // expect packet data is unchanged.
+    EXPECT_EQ(io_recv_data.size(), sizeof(set_block_partial_packet)); 
 }
