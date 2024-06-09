e72a5c5f3f05b0bf47187bbbfde489a9e7d3f909
kirasys <kirasys0@naver.com>
2024-06-09 13:49:59 +0900

    feat: 데이터베이스 mock 클래스 추가

---
+++ mmocraft-test/mock_database.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <atomic>
+
+#define NOMINMAX
+#include <winsock2.h>
+#include <Windows.h>
+#include <sql.h>
+#include <sqlext.h>
+
+namespace test
+{
+    class MockDatabase
+    {
+    public:
+        MockDatabase();
+        ~MockDatabase();
+
+        void set_fetch_fail_once()
+        {
+            fetch_result = false;
+        }
+
+        void set_outbound_integer(SQLINTEGER value)
+        {
+            outbound_integer = value;
+        }
+
+        bool fetch()
+        {
+            auto result = fetch_result;
+            fetch_result = true;
+            return result;
+        }
+
+        void outbound_integer_column(SQLINTEGER& value)
+        {
+            value = outbound_integer;
+        }
+
+        void outbound_integer_column(SQLUINTEGER& value)
+        {
+            value = SQLUINTEGER(outbound_integer);
+        }
+
+    private:
+        bool fetch_result = true;
+        SQLINTEGER outbound_integer = 0;
+    };
+}
