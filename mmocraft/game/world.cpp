37bb77dcbf5830cbea7aacbfb32f45210923629b
kirasys <kirasys0@naver.com>
2024-06-15 05:43:03 +0900

    feat: 맵 데이터, 플레이어를 담당하는 world 클래스 추가

---
+++ mmocraft/game/world.cpp
@@ -0,0 +1,156 @@
+#include "pch.h"
+#include "world.h"
+
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+
+#include "logging/logger.h"
+#include "config/config.h"
+#include "proto/config.pb.h"
+#include "proto/world_metadata.pb.h"
+#include "net/connection_environment.h"
+#include "util/time_util.h"
+#include "util/protobuf_util.h"
+
+#define WORLD_METADATA_FORMAT_VERSION 1
+
+namespace fs = std::filesystem;
+
+namespace game
+{
+    World::World(net::ConnectionEnvironment& a_connection_env)
+        : connection_env{ a_connection_env }
+        , players(connection_env.size_of_max_connections())
+    {
+
+    }
+
+    bool World::add_player(net::ConnectionKey connection_key, 
+                            unsigned player_identity,
+                            game::PlayerType player_type,
+                            const char* username,
+                            const char* password)
+    {
+        bool is_already_logged_in = std::any_of(players.begin(), players.end(),
+            [player_identity](std::unique_ptr<game::Player>& player) {
+                return player && player->identity() == player_identity;
+            }
+        );
+
+        if (not is_already_logged_in) {
+            players[connection_key.index()].reset(new game::Player(
+                connection_key,
+                player_identity,
+                player_type,
+                username,
+                password
+            ));
+        }
+
+        return not is_already_logged_in;
+    }
+
+    void World::on_player_handshake_success(net::ConnectionKey connection_key)
+    {
+        if (auto conn = connection_env.try_acquire_connection(connection_key)) {
+            const auto& server_conf = config::get_server_config();
+
+            net::PacketHandshake handshake_packet{
+                server_conf.server_name(), server_conf.motd(),
+                players[connection_key.index()]->player_type() == game::PlayerType::ADMIN ? net::UserType::OP : net::UserType::NORMAL
+            };
+
+            conn->descriptor.send_handshake_packet(handshake_packet);
+        }
+    }
+
+    bool World::load(const char* a_save_dir)
+    {
+        if (last_saved_tick) {
+            CONSOLE_LOG(error) << "World is already loaded.";
+            return false;
+        }
+
+        // set world files path.
+        save_dir = a_save_dir;
+        block_data_path = save_dir / block_data_filename;
+        world_metadata_path = save_dir / world_metadata_filename;
+        
+        if (not fs::exists(world_metadata_path))
+            create_new_world();
+
+        load_metadata();
+        load_block_data();
+
+        return true;
+    }
+    
+    void World::load_metadata()
+    {
+        util::json_file_to_proto_message(&_metadata, world_metadata_path);
+    }
+
+    void World::load_block_data()
+    {
+        if (not block_mapping.open(block_data_path.string())) {
+            CONSOLE_LOG(error) << "Unalbe to mapping block map";
+            return;
+        }
+    }
+
+    void World::create_new_world() const
+    {
+        const auto& world_conf = config::get_world_config();
+
+        auto map_size = Coordinate3D{ short(world_conf.width()), short(world_conf.height()), short(world_conf.length())};
+        auto map_volume = map_size.x * map_size.y * map_size.z;
+        
+        // write world files to the disk.
+        create_block_file(map_size);
+        create_metadata_file(map_size);
+    }
+
+    void World::create_block_file(Coordinate3D map_size) const
+    {
+        auto blocks_ptr = WorldMapGenerator::generate_flat_world(map_size);
+
+        std::ofstream block_file(block_data_path);
+        block_file.write(blocks_ptr.get(), map_size.x * map_size.y * map_size.z);
+    }
+
+
+    void World::create_metadata_file(Coordinate3D map_size) const
+    {
+        WorldMetadata metadata;
+        metadata.set_format_version(WORLD_METADATA_FORMAT_VERSION);
+        
+        metadata.set_width(map_size.x);
+        metadata.set_height(map_size.y);
+        metadata.set_length(map_size.z);
+        metadata.set_volume(map_size.x * map_size.y * map_size.z);
+
+        metadata.set_spawn_x(map_size.x / 2);
+        metadata.set_spawn_y(map_size.y / 2);
+        metadata.set_spawn_z(map_size.z / 2);
+        metadata.set_spawn_yaw(0);
+        metadata.set_spawn_pitch(0);
+
+        metadata.set_created_at(util::current_timestmap());
+            
+        util::proto_message_to_json_file(metadata, world_metadata_path);
+    }
+
+    std::unique_ptr<BlockID[]> WorldMapGenerator::generate_flat_world(Coordinate3D map_size)
+    {
+        const auto plain_size = map_size.x * map_size.z;
+        auto blocks = new BlockID[plain_size * map_size.y];
+
+        auto num_of_dirt_block = plain_size * (map_size.y / 2);
+        std::memset(blocks, BLOCK_DIRT, num_of_dirt_block);
+        std::memset(blocks + num_of_dirt_block, BLOCK_GRASS, plain_size);
+        std::memset(blocks, BLOCK_BEDROCK, plain_size);
+
+        return std::unique_ptr<BlockID[]>(blocks);
+    }
+}
---
+++ mmocraft/game/world.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <filesystem>
+#include <memory>
+#include <vector>
+
+#include "game/block.h"
+#include "game/player.h"
+#include "proto/world_metadata.pb.h"
+#include "net/connection_key.h"
+#include "win/file_mapping.h"
+#include "util/common_util.h"
+
+namespace net
+{
+    class ConnectionEnvironment;
+}
+
+namespace game
+{
+    constexpr const char* block_data_filename = "blocks.bin";
+    constexpr const char* world_metadata_filename = "metadata.json";
+
+    struct Coordinate3D
+    {
+        short x = 0;
+        short y = 0;
+        short z = 0;
+    };
+
+    class World : util::NonCopyable
+    {
+    public:
+        World(net::ConnectionEnvironment&);
+
+        bool load(const char* save_dir);
+
+        bool add_player(net::ConnectionKey, unsigned player_identity, game::PlayerType, const char* username, const char* password);   
+
+        void on_player_handshake_success(net::ConnectionKey);
+
+    private:
+        void load_metadata();
+
+        void load_block_data();
+        
+        void create_new_world() const;
+
+        void create_block_file(Coordinate3D map_size) const;
+
+        void create_metadata_file(Coordinate3D map_size) const;
+
+        net::ConnectionEnvironment& connection_env;
+
+        std::vector<std::unique_ptr<game::Player>> players;
+
+        WorldMetadata _metadata;
+
+        win::FileMapping block_mapping;
+
+        std::size_t last_saved_tick = 0;
+
+        std::filesystem::path save_dir;
+        std::filesystem::path block_data_path;
+        std::filesystem::path world_metadata_path;
+    };
+
+    class WorldMapGenerator
+    {
+    public:
+        static std::unique_ptr<BlockID[]> generate_flat_world(Coordinate3D map_size);
+    };
+}
