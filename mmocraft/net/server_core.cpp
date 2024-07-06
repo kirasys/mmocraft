e03251ed700452044d0b8e5dc226aa118327b679
kirasys <kirasys0@naver.com>
2024-07-07 00:38:37 +0900

    fix: clean connection 루틴의 주기 변경

--- mmocraft/net/server_core.cpp
+++ mmocraft/net/server_core.cpp
@@ -25,7 +25,7 @@ namespace net
         connection_env_task.schedule(
             util::TaskTag::CLEAN_CONNECTION,
             &ConnectionEnvironment::cleanup_expired_connection,
-            util::MilliSecond(1000));
+            util::MilliSecond(2000));
 
         _state = ServerCore::State::Initialized;
     }
