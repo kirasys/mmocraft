d48216bf79b363100cb4b198e5fdf137d31c07c0
kirasys <kirasys0@naver.com>
2024-07-17 03:41:21 +0900

    fix: password 타입과 동일하게 리턴 타입 수정

--- mmocraft/database/sql/routines/create_functions.sql
+++ mmocraft/database/sql/routines/create_functions.sql
@@ -1,5 +1,5 @@
 CREATE FUNCTION dbo.GetPasswordHash(@password VARCHAR(64))
-RETURNS VARBINARY(64)
+RETURNS BINARY(64)
 AS
 BEGIN
     RETURN HASHBYTES('SHA2_256', '$(PasswordSalt)' + SUBSTRING(@password, 0, LEN(@password) + 1));
